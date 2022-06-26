a7ba2fb2d1dabb1691090a36c4e2f2abe608b4fd
yangyang36 <9723006@nkust.edutw>
2022-06-26 16:52:25 +0800

    paint image, dawOval

--- java-gui/src/Main.java
+++ java-gui/src/Main.java
@@ -5,19 +5,11 @@ public class Main extends JPanel {
     @Override
 //    Java class the paintComponent method when it needs to.
     public void paintComponent(Graphics g){
-//        g.setColor(Color.GREEN);
-//        g.drawLine(100,100,400,400);
-//        g.setColor(Color.red);
-//        g.drawRect(100,100,200,200);
-//        g.fillRect(10,10,50,50);
-        int width = getWidth();
-        int height = getHeight();
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,width,height);
-//        g.fillRect(0,0,300,300); 如果是打這樣 會變成拖拉視窗的時候不會重新執行 paintComponent
+        g.setColor(Color.blue);
+        g.drawOval(0,0,200,200);
 
-        g.setColor(Color.GREEN);
-        g.drawLine(10,10,50,50);
+        ImageIcon img = new ImageIcon("ashi.jpg");
+        img.paintIcon(null,g,150,150);
     }
 
     public static void main(String[] args) {
