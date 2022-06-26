099d309c15ae6cc52607864a69e12e61c339a601
yangyang36 <9723006@nkust.edutw>
2022-06-26 16:46:18 +0800

    setColor, fillRect, set height, set width

--- java-gui/src/Main.java
+++ java-gui/src/Main.java
@@ -3,9 +3,23 @@ import java.awt.*;
 
 public class Main extends JPanel {
     @Override
+//    Java class the paintComponent method when it needs to.
     public void paintComponent(Graphics g){
-        g.drawString("Hello World", 100,100);
+//        g.setColor(Color.GREEN);
+//        g.drawLine(100,100,400,400);
+//        g.setColor(Color.red);
+//        g.drawRect(100,100,200,200);
+//        g.fillRect(10,10,50,50);
+        int width = getWidth();
+        int height = getHeight();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,width,height);
+//        g.fillRect(0,0,300,300); 如果是打這樣 會變成拖拉視窗的時候不會重新執行 paintComponent
+
+        g.setColor(Color.GREEN);
+        g.drawLine(10,10,50,50);
     }
+
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
