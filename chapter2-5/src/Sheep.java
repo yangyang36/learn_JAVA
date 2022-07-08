a3e1496b5ac4cf9a57ec078853a20b0c49b68da2
yangyang36 <9723006@nkust.edutw>
2022-07-08 11:35:59 +0800

    switch

--- chapter2-5/src/Sheep.java
+++ chapter2-5/src/Sheep.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Sheep {
     private int x;
     private int y;
@@ -7,8 +9,21 @@ public class Sheep {
         this.y = y;
     }
 
-    @Override
-    public String toString(){
-        return "The x, y coordinate of this sheep is " + x + " " + y;
+    public void drawSheep (Graphics g){
+        g.setColor(Color.WHITE);
+
+        g.fillOval(x+23, y+50, 225,100);
+        g.fillOval(x, y, 75,75);
+
+        g.fillRect(x+50,y+100,25,100);
+        g.fillRect(x+100,y+100,25,100);
+        g.fillRect(x+150,y+100,25,100);
+        g.fillRect(x+200,y+100,25,100);
+
+    }
+
+    public void walk(int x, int y){
+        this.x += x;
+        this.y += y;
     }
 }
