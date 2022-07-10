1e7ef41340572e45ccb765e98b4e296fba7aee4d
yangyang36 <9723006@nkust.edutw>
2022-07-10 16:23:10 +0800

    Scanner

--- chapter3/src/Main.java
+++ chapter3/src/Main.java
@@ -1,10 +1,35 @@
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        String fileName = JOptionPane.showInputDialog("Filename : ");
+        File f = new File(fileName);
+        int line_count = 0;
+        int word_count=0;
+        int char_count=0;
+        try {
+            Scanner scanner = new Scanner(f);
+            while(scanner.hasNextLine()){
+                line_count ++;
+                String line = scanner.nextLine();
+                String[] line_arr = line.split(" ");
+                for(String word : line_arr){
+                    word_count++;
+                }
+                char_count += line.length();
+            }
+            scanner.close();
+            System.out.println("line count => " + line_count);
+            System.out.println("word count => " + word_count);
+            System.out.println("char count => " + char_count);
 
+        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+            System.out.println("File not found");
+        }
 
-    public static void main(String[] args) throws IllegalArgumentException{
-        Circle c1 = new Circle(10);
-            c1.setRadius(-5);
     }
 }
