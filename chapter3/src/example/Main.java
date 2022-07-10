c0f5797d7475325cb4c411d32aa2c0adac3d8291
yangyang36 <9723006@nkust.edutw>
2022-07-11 01:23:50 +0800

    short circuit

--- chapter3/src/example/Main.java
+++ chapter3/src/example/Main.java
@@ -3,8 +3,15 @@ package example;
 public class Main {
 
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        Outer.Inner inner = outer.new Inner();
-        inner.printI();
+        int n1 = 10;
+        int n2 = 0;
+        boolean whatever = false;
+        if(whatever && (n1/n2) != 3){
+            System.out.println("cannot get here");
+        }
+        if (whatever & (n1/n2) != 3 ){
+            System.out.println("have error");
+        }
+        //Exception in thread "main" java.lang.ArithmeticException: / by zero
     }
 }
