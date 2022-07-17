61cc87150586bd808bd7ee25c7325fc05b163221
yangyang36 <9723006@nkust.edutw>
2022-07-17 22:37:39 +0800

    Parameterized statement

--- learn-JDBC/src/Main.java
+++ learn-JDBC/src/Main.java
@@ -1,32 +1,41 @@
+import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        String sql_statement = "select * from video";
+    private static Connection c;
 
-        // 連接 DB (url, username, pwd)
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/learnJAVA", "ashi", "20190609");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        initializeDB();
+
+        String name = JOptionPane.showInputDialog("Input video name");
+        String sql_statement = "SELECT * FROM video WHERE videoName = ?";
+        PreparedStatement preparedStatement = c.prepareStatement(sql_statement);
+        preparedStatement.setString(1,name);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()){
+            int id = Integer.parseInt(resultSet.getString("videoID"));
+            String videoName = resultSet.getString("videoName");
+            int price = Integer.parseInt(resultSet.getString("price"));
+            JOptionPane.showMessageDialog(null,new Video(id, videoName, price));
+        }else{
+            JOptionPane.showMessageDialog(null, "Video not found.");
+        }
+
+
+        closeDB();
+    }
+
+    private static void initializeDB() throws SQLException {
+        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/learnJAVA", "ashi", "20190609");
         if(c != null){
             System.out.println("Connecting ...");
         }else {
             System.out.println("Cannot connect to the database");
         }
+    }
 
-        PreparedStatement pps = c.prepareStatement(sql_statement);
-
-        // 執行 line 5 的 sql
-        ResultSet rs = pps.executeQuery();
-
-        ArrayList<Video> result = new ArrayList<>();
-        while(rs.next()){
-            Video v = new Video(Integer.parseInt(rs.getString("videoId")), rs.getString("videoName"), Integer.parseInt(rs.getString("price")));
-            result.add(v);
-        }
-
-        for(Video v : result){
-            System.out.println(v.toString());
-        }
+    private static void closeDB() throws SQLException {
         c.close();
     }
 }
