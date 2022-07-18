a52072da5664c30794fd0af468ef35bb1873e410
yangyang36 <9723006@nkust.edu.tw>
2022-07-19 01:31:18 +0800

    handling get request, and servlet life cycle

--- webServer/src/main/java/myCode/TryServlet.java
+++ webServer/src/main/java/myCode/TryServlet.java
@@ -8,13 +8,36 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "Tryervlet", value = "/Tryervlet")
 public class TryServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException{
+        super.init();
+        System.out.println("init method ...");
+//        １
+    }
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        super.service(request, response);
+        System.out.println("service method ...");
+//        3
+    }
+
+    @Override
+    public void destroy(){
+        super.destroy();
+        System.out.println("destroy method...");
+//        server 重開的時候才會執行，而不是網頁關掉的時候
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGET method...");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<p>This is TryServlet</p>");
         out.println("</body></html>");
+//  2
 
     }
 
---
+++ webServer/src/main/webapp/form/myform.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huiyuanyang
+  Date: 2022/7/19
+  Time: 01:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="http://localhost:8080/formHandling" method="get">
+    <label for="name">Book name: </label>
+    <input type="text" name="name" id="name">
+    <br>
+    <label for="price">Price: </label>
+    <input type="number" name="price" id="price">
+    <br>
+    <label for="author">Author: </label>
+    <input type="text" name="author" id="author">
+
+    <button type="submit">SUBMIT</button>
+
+</form>
+</body>
+</html>
